bb6d2d60e9b46244375ece0ef54ebc7bcd5a9b3c
Vera Stepanova <73222328+VeraStepanova@users.noreply.github.com>
2025-12-10 18:26:53 +0100

    update

--- tests/assesibility.test.ts
+++ tests/assesibility.test.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { injectAxe, checkA11y } from 'axe-playwright';
 import { StorePage } from '../pages/storePage';
-import { LoginPage } from '../pages/loginPage';
 
 test('store page accessibility check', async ({ page }) => {
   const store = new StorePage(page);
